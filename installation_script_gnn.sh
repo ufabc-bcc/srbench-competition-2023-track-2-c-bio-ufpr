a1db8ca37d21badb823ede11ed1ed4cc147a4d9e
martins6 <adrielfalcao@gmail.com>
2023-05-20 14:51:27 -0300

    feat: adding new installation scripts

---
+++ installation_script_gnn.sh
@@ -0,0 +1,2 @@
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cpu.html
